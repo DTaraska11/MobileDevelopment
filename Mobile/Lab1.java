0a0b396e9d7706f460afafa99c924ac104ebfd1e
DTaraska11 <56418437+DTaraska11@users.noreply.github.com>
2020-09-09 14:44:04 -0400

    Update Lab1.java

--- Mobile/Lab1.java
+++ Mobile/Lab1.java
@@ -14,12 +14,12 @@ public class Lab1 {
     int sLength= myObj.nextint();
     System.out.println("/n Height for square: ");
     int sHeight= myObj.nextInt();
-    square.SetDimensions(sLength, sHeight);
+    square.setDimensions(sLength, sHeight);
 
     
     System.out.println("/n Radius for circle: ");
     int radius = myObj.nextInt();
-    circle.SetDimensions(radius);
+    circle.setDimensions(radius);
 
    
     System.out.println("/n Side1 for triangle: ");
@@ -28,17 +28,22 @@ public class Lab1 {
     int side2= myObj.nextInt();
     System.out.println("/n Side3 for triangle: ");
     int side3= myObj.nextInt();
-    triangle.SetDimensions(side1, side2, side3);
+    triangle.setDimensions(side1, side2, side3);
 
     
     System.out.println("/n Side for equilateral triangle: ");
     int side= myObj.nextInt();
-    ETriangle.SetDimensions(side);
+    ETriangle.setDimensions(side);
 
-    square.PrintDimensions();
-    circle.PrintDimensions();
-    triangle.PrintDimensions();
-    ETriangle.PrintDimensions();
+    square.printDimensions();
+    circle.printDimensions();
+    triangle.printDimensions();
+    ETriangle.printDimensions();
+    System.out.println(square.getArea());
+    System.out.println(circle.getArea());
+    System.out.println(triangle.getArea());
+    System.out.println(ETriangle.getArea());
+   
     
 }
 }
